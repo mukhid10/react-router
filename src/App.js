49b384d4a3f61f4ed9ed7d8cbaa5863f5cdc67ca
mukhid10 <mukhid976@gmail.com>
2022-03-13 09:02:01 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import MovieList from "./pages/MovieList";
+import NotFound from "./pages/NotFound";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="*" element={<NotFound/>}/> {/* Route jika URL path yang dicari tidak ada dan akan menuju component Not Found */}
+          <Route path="/movies/:id" element={<Movie/>}/> {/* Route movie dengan id Param yang akan menuju ke halaman pada component pages/Movie */}
+          <Route path="/" element={<Home/>}/> {/* Route homepage yang akan menuju ke halaman pada component pages/Home */}
+          <Route path="/movies" element={<MovieList/>}/> {/* Route movies yang akan menuju ke halaman pada component pages/MovieList */}
+        </Routes>
+      </div>
+    </>
   );
 }
-
-export default App;
